c477ac6693c484e99ba64abbab5246655fb3ccd1
Admin <Admin@STUDENT09>
2019-01-31 09:48:00 +0000

    Made Account an entity and added config files.

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -1,9 +1,21 @@
 package com.qa.persistence.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Account {
 
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(length = 20)
 	private String firstName;
+	@Column(length = 20)
 	private String lastName;
+	@Column(length = 4)
 	private int accountNumber;
 	
 	public Account(String firstName, String lastName, int accountNumber) {
